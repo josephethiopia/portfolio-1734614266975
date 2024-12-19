49e3d1350baddcc597dae4dfefc366124d634c11
Joseph___ <118039462+josephethiopia@users.noreply.github.com>
2024-12-19 16:18:10 +0300

    Add app/skills/page.tsx

---
+++ app/skills/page.tsx
@@ -0,0 +1,7 @@
+
+import { content } from '../../content/data'
+import SkillsComponent from '../../components/Skills'
+
+export default function SkillsPage() {
+    return <SkillsComponent content={content.skills} />
+}
