d0e0ac6bb9d6824ee77618e49046e6dc667cbb0f
Joseph___ <118039462+josephethiopia@users.noreply.github.com>
2024-12-19 16:18:12 +0300

    Add app/contact/page.tsx

---
+++ app/contact/page.tsx
@@ -0,0 +1,7 @@
+
+import { content } from '../../content/data'
+import ContactComponent from '../../components/Contact'
+
+export default function ContactPage() {
+    return <ContactComponent content={content.contact} />
+}
